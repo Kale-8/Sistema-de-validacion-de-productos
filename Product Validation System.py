82327cc280523e16d397cae65abd416c7737cd30
Kale-8 <61241903+Kale-8@users.noreply.github.com>
2025-04-24 07:55:15 -0500

    feat: dot notation added

--- Product Validation System.py
+++ Product Validation System.py
@@ -1,4 +1,5 @@
-
+# Se importa el modulo types para usar dot notation
+from types import SimpleNamespace
 # Se crea una funcion para alertar errores
 def alert():
         print('\nEl numero ingresado es incorrecto, ingresalo nuevamente')
@@ -17,14 +18,14 @@ finalCost = 0
 # Se crea un ciclo para habilitar la compra de 2 o mas productos
 while True:
 # Se crea un diccionario donde se guardaran los datos del producto
-    fullProduct = {}
+    fullProduct = SimpleNamespace(**{})
 # Se declaran las llaves necesarias
-    fullProduct['product'] = str(input('\n-------> Ingresa el nombre del producto: '))
-    fullProduct['price'] = validation('\n-------> Ingresa el precio del producto:', lambda x: x > 0)
-    fullProduct['amount'] = validation('\n-------> Cuantos productos deseas comprar: ', lambda x: x > 0)
-    fullProduct['discount'] = validation('\n-------> Ingresa el descuento que tiene el producto: ', lambda x: 0 <= x <= 100)
-    fullProduct['sub-total'] = float(fullProduct['price'] * fullProduct['amount'])
-    fullProduct['full-price'] = float(fullProduct['sub-total'] - (fullProduct['sub-total'] * (fullProduct['discount']/100)))
+    fullProduct.product = str(input('\n-------> Ingresa el nombre del producto: '))
+    fullProduct.price = validation('\n-------> Ingresa el precio del producto:', lambda x: x > 0)
+    fullProduct.amount = validation('\n-------> Cuantos productos deseas comprar: ', lambda x: x > 0)
+    fullProduct.discount = validation('\n-------> Ingresa el descuento que tiene el producto: ', lambda x: 0 <= x <= 100)
+    fullProduct.sub_total = float(fullProduct.price * fullProduct.amount)
+    fullProduct.full_price = float(fullProduct.sub_total - (fullProduct.sub_total * (fullProduct.discount/100)))
 # Se añade el producto a la lista
     productsList.append(fullProduct)
 # Se valida si el usuario desea comprar otro producto
@@ -36,13 +37,13 @@ print('='*40)
 print('DETALLE DE FACTURACIÓN')
 print('='*40)
 for product in productsList:
-    finalCost += product['full-price']
-    print(f'Producto       : {product['product']}')
-    print(f'Cantidad       : {product['amount']}')
-    print(f'Precio unitario: ${product['price']:.2f}')
-    print(f'Subtotal       : ${product['sub-total']:.2f}')
-    print(f'Descuento ({product['discount']}%): -${product['sub-total'] * (product['discount']/100):.2f}')
-    print(f'Total          : ${product['full-price']:.2f}')
+    finalCost += product.full_price
+    print(f'Producto       : {product.product}')
+    print(f'Cantidad       : {product.amount}')
+    print(f'Precio unitario: ${product.price:.2f}')
+    print(f'Subtotal       : ${product.sub_total:.2f}')
+    print(f'Descuento ({product.discount}%): -${product.sub_total * (product.discount/100):.2f}')
+    print(f'Total          : ${product.full_price:.2f}')
     print('='*40)
 print(f'Costo final    : ${finalCost:.2f}')
 print('='*40)
